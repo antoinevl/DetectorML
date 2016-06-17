3e3ce94a24d47d408426949c0f473369f2f8d7ca
avl <avl@debian>
2016-06-17 18:18:36 +0100

    Added DB features

--- Extractor/extractor.py
+++ Extractor/extractor.py
@@ -2,9 +2,9 @@
 
 import urllib2
 
-url = 'http://www.google.com/'
+url_name = 'http://www.google.com/'
 
-resp = urllib2.urlopen(url)
+resp = urllib2.urlopen(url_name)
 page = resp.read()
 
 
@@ -20,6 +20,54 @@ while (page.find(script_token_start, ind_start)!=-1):
 	javascript.append(page[ind_start:ind_end])
 	ind_start = ind_end
 
-print(javascript[0])
+# Extract static features
+static_features = {}
 
-# 
+lines = page.splitlines()
+static_features['line_count'] = len(lines)
+
+letter_count = 0
+for line in lines:
+	letter_count += len(line)
+static_features['letter_count'] = letter_count
+
+word_count = 0
+for line in lines:
+	word_count += len(line.split())
+static_features['word_count'] = word_count
+
+# Extract dynamic features
+dynamic_features = {}
+dynamic_features['feat1'] = 0
+dynamic_features['feat2'] = 0
+
+# Save in database
+from pymongo import MongoClient
+client = MongoClient()
+db = client.projectDB
+urls = db.urls
+
+def insert_url(url_name, description, url_type, static_features_dic, dynamic_features_dic, collection):
+	if len(collection.find_one({"url":url_name}))>2:
+		print("URL: "+url_name+" already stored in the collection.")
+	else:
+		s = {
+			"url": url_name,
+			"description": description,
+			"type": url_type,
+			"static_features": [{
+				"line_count": static_features['line_count'],
+				"word_count": static_features['word_count'],
+				"letter_count": static_features['letter_count']
+			}],
+			"dynamic_features": [{
+				"feat1": 0,
+				"feat2": 0
+			}]
+		     }
+
+		result = collection.insert_one(s)
+		print("URL added to the collection.")
+		return result
+
+insert_url(url_name, 'Test', 'Benign', static_features, dynamic_features, urls)
---
+++ MongoDB/README.md
@@ -0,0 +1,7 @@
+[Tutorial MongoDB](http://www.tutorialspoint.com/mongodb/)
+mongo
+use projectDB
+db.createCollection("features.....")
+
+
+[Tutorial PyMongo](http://api.mongodb.com/python/current/tutorial.html#bulk-inserts)
