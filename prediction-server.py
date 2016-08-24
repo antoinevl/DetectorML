6dfaa3b3e8a2cef46bb097be723710a43e57a86a
antoinevl <antoinevl@free.fr>
2016-08-24 21:31:29 +0100

    Update server

--- prediction-server.py
+++ prediction-server.py
@@ -1,10 +1,10 @@
-from bottle import route, run, template
+from bottle import route, run, template, request
 from base64 import b64encode, b64decode
 from detector import predict
 
-@route('/prediction/<urlb64>')
-def f(urlb64):
-	url = b64decode(urlb64)
-	return "URL: "+url+"\nPrediction:"+predict(url)+"."
+@route('/prediction')
+def f():
+	url = request.query.url
+	return "URL: "+url+"<br>Prediction:"+predict(url)+"."
 
 run(host='146.169.47.251', port=8080)
