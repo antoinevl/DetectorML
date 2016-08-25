aa9dd7c2fa13e80537296f524460cc5d7d76381f
Antoine V <amv15@ic.ac.uk>
2016-08-25 15:53:25 +0100

    Update extension and server

--- Firefox-Extension/detector.js
+++ Firefox-Extension/detector.js
@@ -1,1 +1,10 @@
-alert('AAAAAAAAAAAAAAAAAAAA');
+function httpGet(theUrl){
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+var url = document.URL
+var detector_url = "http://146.169.47.251:8080/prediction?url="+url
+alert('Result: '+httpGet(detector_url));
--- prediction-server.py
+++ prediction-server.py
@@ -1,10 +1,25 @@
-from bottle import route, run, template, request
+from bottle import route, run, template, request, response
 from base64 import b64encode, b64decode
 from detector import predict
+import bottle
+
+def enable_cors(fn):
+    def _enable_cors(*args, **kwargs):
+        # set CORS headers
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Ty$
+
+        if bottle.request.method != 'OPTIONS':
+            # actual request; reply with the actual response
+            return fn(*args, **kwargs)
+
+    return _enable_cors
 
 @route('/prediction')
+@enable_cors
 def f():
-	url = request.query.url
-	return "URL: "+url+"<br>Prediction:"+predict(url)+"."
+        url = request.query.url
+        return "URL: "+url+"<br>Prediction:"+predict(url)+"."
 
 run(host='146.169.47.251', port=8080)
