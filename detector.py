56959c105ddef112f4749a8b66df3263c3dfd204
Antoine V <amv15@ic.ac.uk>
2016-08-27 13:50:19 +0100

    Update server

--- detector.py
+++ detector.py
@@ -14,7 +14,7 @@ url_name = "http://fahd.com"
 # Make sure that phantomjs is launched by the command: phantomjs  --webdriver 28042
 
 def predict(url_name):
-    features_name_file = "feat_names.pkl"
+    features_name_file = "Dumps/feat_names.pkl"
     clf = joblib.load("Dumps/clf_svm.pkl")
     X_to_predict = url_to_X(url_name, features_name_file)
     prediction =  clf.predict(X_to_predict)[0]
