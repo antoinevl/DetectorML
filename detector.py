36b7528ad6ef322df6d84d91b466940ded556058
avl <antoinevl@free.fr>
2016-09-01 15:12:47 +0100

    Update

--- detector.py
+++ detector.py
@@ -7,7 +7,11 @@ Created on Wed Aug 24 16:52:50 2016
 
 from sklearn.externals import joblib
 from Extractor.url_processing import url_to_X
-from pprint import pprint
+import time
+from Crawler.crawler import urls_from_crawler
+from Crawler.crawler import get_fields_from_malicious_file
+import sys
+import matplotlib.pyplot as plt
 
 # Make sure that phantomjs is launched by the command: phantomjs  --webdriver 28042
 
@@ -21,6 +25,47 @@ def predict(url_name):
     else:
         return "malicious"
 
-if __name__=='__main__':
+def predict_urls(l):
+    n = len(l)
+    cpt = 0
+    times = []
+    sys.stdout.write("Prediction: ")
+    for u in l:
+        t0 = time.time()
+        predict(u)
+        cpt += 1
+        sys.stdout.write("\rPrediction: "+str(cpt*100/n)+"%    ")
+        t1 = time.time()
+        t = t1-t0
+        times.append(t)
+    return times
+    
+def stat_predict_urls(times):
+    t_total = sum(times)
+    average_time = t_total/len(times)
+    print "Average time: "+average_time+"."
+    plt.figure() 
+    plt.hist(times,10)
+    plt.ylabel("# URLs")
+    plt.xlabel("Time (seconds)")
+    
+    
+def test_predict():
     url_name = "http://fahd.com"
     print predict(url_name)
+
+def test_predict_urls():
+    benign_urls_addr = '/home/avl/MSc-project/Crawler/alexa-top500'
+    malicious_urls_addr = '/home/avl/MSc-project/Crawler/mwlist_all'
+    burls = urls_from_crawler(benign_urls_addr)
+    mfields = get_fields_from_malicious_file(malicious_urls_addr)
+    murls = [f['url_name'] for f in mfields]
+    urls = burls+murls
+    print "Start...\n"
+    predict_urls(urls)
+
+
+
+if __name__=='__main__':
+#    test_predict()
+    test_predict_urls()
