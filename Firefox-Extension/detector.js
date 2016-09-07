74e8a89ab475020771a261dbb081308e3bc71bbb
Antoine V <amv15@ic.ac.uk>
2016-09-07 15:23:06 +0100

    Update

--- Firefox-Extension/detector.js
+++ Firefox-Extension/detector.js
@@ -7,26 +7,9 @@ function httpGet(theUrl){
 
 function textDisplay(theUrl){
     var res = httpGet(theUrl);
-    var type;
-    var proba;
-    if (res.search("benign") != -1){
-        type = "benign";
-    } else if (res.search("malicious") != -1){
-        type = "malicious";
-    } else {
-		type = "error";
-    }
-    if (res.search("benign") != -1){
-        type = "benign";
-    } else if (res.search("malicious") != -1){
-        type = "malicious";
-    } else {
-		type = "error";
-    }
-        
     return res;
 }
 
 var url = document.URL
 var detector_url = "http://146.169.47.251:8080/prediction?url="+url
-alert('Result: '+textDisplay(detector_url));
+alert(textDisplay(detector_url));
